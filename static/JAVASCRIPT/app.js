6c060891ae155b34ecd46569e1e1b4d6fd9bbb16
Muhammad Qasim <malikqasim20051@gmail.com>
2024-07-09 13:06:36 +0500

    Booking-Schedule-Content

--- static/JAVASCRIPT/app.js
+++ static/JAVASCRIPT/app.js
@@ -9,6 +9,47 @@ function menuAnimation(x) {
   }
 
 
+
+
+
+
+    // Function to show datepicker on input click
+   // Function to show datepicker on input click
+   function showDatepicker(input) {
+    input.type = 'date';
+    input.focus();
+}
+
+// Handle focus events to manage placeholder display
+document.addEventListener('DOMContentLoaded', function() {
+    const dateInputs = document.querySelectorAll('.date-input');
+    dateInputs.forEach(input => {
+        const defaultPlaceholder = input.placeholder;
+        input.addEventListener('focus', function() {
+            this.placeholder = '';
+            this.type = 'date'; // Ensure input type remains 'date' on focus
+        });
+        input.addEventListener('blur', function() {
+            if (!this.value) {
+                this.placeholder = defaultPlaceholder;
+                this.type = 'text'; // Reset to text input type if no date selected
+            }
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+  
 // OUR CARS DROP-DOWN
 
 // Hero Section Schedule Section
+
